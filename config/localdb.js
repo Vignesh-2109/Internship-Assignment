524e29fd3b8beaa423db8417f96be51c0c767410
Vignesh_Maram <130981939+Vignesh-2109@users.noreply.github.com>
2024-02-23 20:45:58 +0530

    Delete config directory

--- config/localdb.js
+++
@@ -1,6 +0,0 @@
-
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/Vigesh_admin_dashboard');
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
